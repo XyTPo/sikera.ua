77d922a011d3c72b66c40dfed27cb0814f859ff4
XyTPo <mekhed.r@gmail.com>
2019-06-26 10:53:39 +0300

    footer upd

--- wp-content/themes/sikera/footer.php
+++ wp-content/themes/sikera/footer.php
@@ -8,13 +8,37 @@
         </div>
         <div class="footer__links">
             <a class="footer__links-item" href="/">
-                <span class="foote__links-icon">           
-                    <svg  class="ls-stscreen__mouse-svg">
-                        <use xlink:href="si-facebook"></use>
+                <span class="footer__links-icon">           
+                    <svg  class="footer__links-icon-phone">
+                        <use xlink:href="#si-phone"></use>
                     </svg>
                 </span>
-                <span class="foote__links-icon">
+                <span class="footer__links-icon">
                     +38(097)683-7904
+                </span>
+            </a>
+            <a class="footer__links-item" href="/">
+                <span class="footer__links-icon">           
+                    <svg  class="footer__links-icon-mail">
+                        <use xlink:href="#si-mail"></use>
+                    </svg>
+                </span>
+                <span class="footer__links-text">
+                    office.honeyspas@gmail.com
+                </span>
+            </a>
+            <a class="footer__links-item" href="/">
+                <span class="footer__links-icon">           
+                    <svg  class="footer__links-icon-instagram">
+                        <use xlink:href="#si-instagram"></use>
+                    </svg>
+                </span>
+            </a>
+            <a class="footer__links-item" href="/">
+                <span class="footer__links-icon">           
+                    <svg  class="footer__links-icon-facebook">
+                        <use xlink:href="#si-facebook"></use>
+                    </svg>
                 </span>
             </a>
         </div>
